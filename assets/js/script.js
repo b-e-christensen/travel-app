8dc9fc690de8867d54415640cac2a0d3820a2c17
anon123123123 <cditodavis96@gmail.com>
2022-02-03 23:00:38 -0700

    Added maps func - pulling some nearby location data via PLaces API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,10 @@
 let autocomplete;
 function initAutocomplete() {
     autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('search-place'), 
+        document.getElementById('search-place'),
         {
-        types: ['geocode'],
-        fields: ['place_id', 'geometry', 'name']
+            types: ['geocode'],
+            fields: ['place_id', 'geometry', 'name', 'photo']
         })
 }
 
@@ -14,30 +14,71 @@ function initAutocomplete() {
 function searchFormHandler(event) {
     event.preventDefault()
     let place = autocomplete.getPlace()
-    if(!place.geometry) {
+    if (!place.geometry) {
         document.getElementById('search-place').placeholder = 'Enter a place:'
     } else {
         console.log(place)
-        /* This is the Object place 
-{
-    "geometry": {
-        "location": {
-            "lat": 39.7392358,
-            "lng": -104.990251
-        },
-        "viewport": {
-            "south": 39.61443097092009,
-            "west": -105.1099269730715,
-            "north": 39.91424694425041,
-            "east": -104.6002959250625
-        }
-    },
-    "name": "Denver",
-    "place_id": "ChIJzxcfI6qAa4cR1jaKJ_j0jhE",
-    "html_attributions": []
-}
-*/
     }
+    let lat = place.geometry.location.lat()
+    let lon = place.geometry.location.lng()
+    document.getElementById('place-img').src = place.photos[0].getUrl()
+    initialize(lat, lon)
+}
+
+// https://developers.google.com/maps/documentation/javascript/places#place_search_requests init may and get nearby attractions
+let map;
+let service;
+let infowindow;
+function initialize(lat, lon) {
+    let locationSearched = new google.maps.LatLng(lat, lon);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: locationSearched,
+        zoom: 15
+    });
+
+    let request = {
+        location: locationSearched,
+        radius: '300',
+        query: 'attraction'
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+}
+
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            var place = results[i];
+            createPhotoMarker(results[i]);
+            // console.log(place)
+        }
+    }
+}
+
+function createPhotoMarker(place) {
+    var photos = place.photos;
+    if (!photos) {
+        return;
+    }
+
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location,
+        title: place.name,
+        icon: photos[0].getUrl({ maxWidth: 35, maxHeight: 35 })
+    });
+}
+//  Function to set a marker UNTESTED
+function setMarker(lati,long) {
+    const myLatLng = { lat: lati, lng: long };
+    new google.maps.Marker({
+        position: myLatLng,
+        map,
+        title: "Hello World!",
+      });
+      marker.setMap(map);
 }
 
 // Event Listener for search form
